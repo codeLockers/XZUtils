1060351ba3dbb64de21ba1633fb6ab85c9354d5d
codeLocker <codeLockers@outlook.com>
2017-06-26 19:43:26 +0800

    update Category.h

--- XZUtils/XZUtils/Category/XZCategory.h
+++ XZUtils/XZUtils/Category/XZCategory.h
@@ -9,27 +9,22 @@
 #ifndef XZCategory_h
 #define XZCategory_h
 
-
+#import "NSArray+XZJson.h"
+#import "NSDate+XZFormat.h"
+#import "NSDate+XZComponent.h"
+#import "NSDate+XZFunction.h"
+#import "NSDictionary+XZJson.h"
+#import "UIScreen+XZSize.h"
 #import "UIColor+XZGenerate.h"
-
 #import "UIView+XZFrame.h"
-
 #import "UINavigationController+XZNavigationBar.h"
-
 #import "UIImage+XZBitmap.h"
-
-
-
+#import "NSString+XZSize.h"
 #import "NSAttributedString+XZGenerate.h"
-
-
-
-
-
 #import "UIDevice+XZInfo.h"
-
 #import "UITextField+XZProperty.h"
-
 #import "NSBundle+XZApp.h"
+#import "NSUserDefaults+XZStorage.h"
+
 
 #endif /* XZCategory_h */
